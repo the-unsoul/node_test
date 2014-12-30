694a7262f015203a5cb1590240179a10dea1e2e6
UnS <duylinh20032003@yahoo.com>
2014-12-30 10:47:48 +0700

    upgrade with responsve server

--- example.js
+++ example.js
@@ -1,9 +1,23 @@
 var http = require('http');
 
 var server = http.createServer(function (req, res) {
+	var h = new helper();
 	console.log('\n oy! I just got a request\n');
-	res.end("hey this is my node server");
+	res.end("hey this is my node server: \n");
+	h.printObject(req);
 });
 
 server.listen(8080);
 
+function helper(){
+}
+helper.prototype.lognow = function(){
+	console.log('test');
+};
+helper.prototype.printObject = function(o){
+	var out = '';
+  	for (var p in o) {
+    	out += p + ': ' + o[p] + '\n';
+  	}
+  	console.log(out);
+};
