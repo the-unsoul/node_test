6c96670c7dfed5ee42144954acf47b2ab0f624ec
Linh PD <linhpd@elinext.com>
2015-01-09 12:23:03 +0700

    update server

--- example.js
+++ example.js
@@ -1,23 +1,26 @@
 var http = require('http');
+var h = new helper(3300);
 
+/**
+ *
+ * create simple nodejs server
+ * @param  {Object} req  Request object
+ * @param  {Object} res  Response object		
+ */
 var server = http.createServer(function (req, res) {
-	var h = new helper();
 	console.log('\n oy! I just got a request\n');
 	res.end("hey this is my node server: \n");
 	h.printObject(req);
 });
 
-server.listen(8080);
+console.log("server listen to current port: "+h.port);
+server.listen(h.port);
 
-function helper(){
-}
-helper.prototype.lognow = function(){
-	console.log('test');
-};
-helper.prototype.printObject = function(o){
-	var out = '';
-  	for (var p in o) {
-    	out += p + ': ' + o[p] + '\n';
-  	}
-  	console.log(out);
-};
+
+/**
+ * helper containt stuff to use with server
+ * @return port: 3000 !default
+ * @printObject: print contents of an object
+ */
+function helper(port){this.port = port;}
+helper.prototype.printObject = function(o){var out = ''; for (var p in o) {out += p + ': ' + o[p] + '\n'; } console.log(out); };
---
+++ start.bat
@@ -0,0 +1,6 @@
+::@REM @Author: UnS
+::@REM @Date:   2015-01-09 11:53:51
+::@REM @Last Modified by:   UnS
+::@REM Modified time: 2015-01-09 11:53:51
+
+node example.js
